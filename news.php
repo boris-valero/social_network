bcf3e3e0d61dec386adb82da7dad950cab1a8063
boris-valero <157314735+boris-valero@users.noreply.github.com>
2024-12-04 15:34:26 +0100

    Update News.php

--- news.php
+++ news.php
@@ -35,18 +35,7 @@
             </aside>
             <main>
                 <?php
-                /*
-                  // C'est ici que le travail PHP commence
-                  // Votre mission si vous l'acceptez est de chercher dans la base
-                  // de données la liste des 5 derniers messsages (posts) et
-                  // de l'afficher
-                  // Documentation : les exemples https://www.php.net/manual/fr/mysqli.query.php
-                  // plus généralement : https://www.php.net/manual/fr/mysqli.query.php
-                 */
-
-                // Etape 1: Ouvrir une connexion avec la base de donnée.
                 $mysqli = new mysqli("localhost", "root", "Jvale2lppsc", "socialnetwork");
-                //verification
                 if ($mysqli->connect_errno)
                 {
                     echo "<article>";
