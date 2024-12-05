0ea60ddd5e485d8d14a770727696cf2e3eb00573
boris-valero <157314735+boris-valero@users.noreply.github.com>
2024-12-05 11:56:45 +0100

    Update Followers.php

--- followers.php
+++ followers.php
@@ -38,11 +38,8 @@
             </aside>
             <main class='contacts'>
                 <?php
-                // Etape 1: récupérer l'id de l'utilisateur
                 $userId = intval($_GET['user_id']);
-                // Etape 2: se connecter à la base de donnée
-                $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
-                // Etape 3: récupérer le nom de l'utilisateur
+                $mysqli = new mysqli("localhost", "root", "Jvale2lppsc", "socialnetwork");
                 $laQuestionEnSql = "
                     SELECT users.*
                     FROM followers
@@ -51,14 +48,19 @@
                     GROUP BY users.id
                     ";
                 $lesInformations = $mysqli->query($laQuestionEnSql);
-                // Etape 4: à vous de jouer
-                //@todo: faire la boucle while de parcours des abonnés et mettre les bonnes valeurs ci dessous 
-                ?>
-                <article>
-                    <img src="user.jpg" alt="blason"/>
-                    <h3>Béatrice</h3>
-                    <p>id:321</p>
-                </article>
+                if (!$lesInformations) {
+                        echo "Échec de la requête : " . $mysqli->error;
+                }
+                while ($abonne = $lesInformations->fetch_assoc()) {
+                    ?>                
+                    <article>
+                        <img src="user.jpg" alt="blason"/>
+                        <h3><?php echo htmlspecialchars($abonne['alias']); ?></h3>
+                        <p>id:<?php echo intval($abonne['id']); ?></p>                    
+                    </article>
+                <?php 
+                    } 
+                    ?>
             </main>
         </div>
     </body>
