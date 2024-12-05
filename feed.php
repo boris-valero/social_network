48e3db8ad8ccf873da135b2a92af5efa81363cc9
boris-valero <157314735+boris-valero@users.noreply.github.com>
2024-12-05 11:09:04 +0100

    Update Feed.php

--- feed.php
+++ feed.php
@@ -27,39 +27,22 @@
         </header>
         <div id="wrapper">
             <?php
-            /**
-             * Cette page est TRES similaire à wall.php. 
-             * Vous avez sensiblement à y faire la meme chose.
-             * Il y a un seul point qui change c'est la requete sql.
-             */
-            /**
-             * Etape 1: Le mur concerne un utilisateur en particulier
-             */
             $userId = intval($_GET['user_id']);
             ?>
             <?php
-            /**
-             * Etape 2: se connecter à la base de donnée
-             */
-            $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+            $mysqli = new mysqli("localhost", "root", "Jvale2lppsc", "socialnetwork");
             ?>
-
             <aside>
                 <?php
-                /**
-                 * Etape 3: récupérer le nom de l'utilisateur
-                 */
                 $laQuestionEnSql = "SELECT * FROM `users` WHERE id= '$userId' ";
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 $user = $lesInformations->fetch_assoc();
-                //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
-                echo "<pre>" . print_r($user, 1) . "</pre>";
                 ?>
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
                 <section>
                     <h3>Présentation</h3>
                     <p>Sur cette page vous trouverez tous les message des utilisatrices
-                        auxquel est abonnée l'utilisatrice XXX
+                        auxquel est abonnée l'utilisatrice <?php echo htmlspecialchars($user['alias']); ?>
                         (n° <?php echo $userId ?>)
                     </p>
 
@@ -67,9 +50,6 @@
             </aside>
             <main>
                 <?php
-                /**
-                 * Etape 3: récupérer tous les messages des abonnements
-                 */
                 $laQuestionEnSql = "
                     SELECT posts.content,
                     posts.created,
@@ -91,35 +71,25 @@
                 {
                     echo("Échec de la requete : " . $mysqli->error);
                 }
-
-                /**
-                 * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
-                 * A vous de retrouver comment faire la boucle while de parcours...
-                 */
-                ?>                
-                <article>
-                    <h3>
-                        <time datetime='2020-02-01 11:12:13' >31 février 2010 à 11h12</time>
-                    </h3>
-                    <address>par AreTirer</address>
-                    <div>
-                        <p>Ceci est un paragraphe</p>
-                        <p>Ceci est un autre paragraphe</p>
-                        <p>... de toutes manières il faut supprimer cet 
-                            article et le remplacer par des informations en 
-                            provenance de la base de donnée</p>
-                    </div>                                            
-                    <footer>
-                        <small>♥ 132</small>
-                        <a href="">#lorem</a>,
-                        <a href="">#piscitur</a>,
-                    </footer>
-                </article>
-                <?php
-                // et de pas oublier de fermer ici vote while
+                while ($post = $lesInformations->fetch_assoc())
+                {
+                    ?>                
+                    <article>
+                        <h3>
+                            <time><?php echo htmlspecialchars($post['created']); ?></time>
+                        </h3>
+                        <address><?php echo htmlspecialchars($post['author_name']); ?></address>
+                        <div>
+                            <p><?php echo nl2br(htmlspecialchars($post['content'])); ?></p>
+                        </div>                                            
+                        <footer>
+                            <small><?php echo intval($post['like_number']); ?> J'aime</small>
+                            <a href="#"><?php echo htmlspecialchars($post['taglist']); ?></a>
+                        </footer>
+                    </article>
+                <?php 
+                } 
                 ?>
-
-
             </main>
         </div>
     </body>
