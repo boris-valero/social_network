1cfe8cfb4d86f0784d7a6e2ddbc0eabb3bd1f9d4
boris-valero <157314735+boris-valero@users.noreply.github.com>
2024-12-05 22:02:29 +0100

    Refactoring Header & Footer for each files.

--- tags.php
+++ tags.php
@@ -1,29 +1,4 @@
-<!doctype html>
-<html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>ReSoC - Les message par mot-clé</title> 
-        <meta name="author" content="Julien Falconnet">
-        <link rel="stylesheet" href="style.css"/>
-    </head>
-    <body>
-        <header>
-            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
-            <nav id="menu">
-                <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
-                <a href="tags.php?tag_id=1">Mots-clés</a>
-            </nav>
-            <nav id="user">
-                <a href="#">Profil</a>
-                <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
-                </ul>
-            </nav>
-        </header>
+<?php $pageTitle = "Les mots-clés"; include 'header.php'; ?> 
         <div id="wrapper">
             <?php
             $tagId = intval($_GET['tag_id']);
@@ -37,7 +12,7 @@
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 $tag = $lesInformations->fetch_assoc();
                 ?>
-                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
+                <img src="user.jpg" alt="Portrait de l'utilisateur"/>
                 <section>
                     <h3>Présentation</h3>
                     <p>Sur cette page vous trouverez les derniers messages comportant
@@ -96,6 +71,4 @@
                 } 
                 ?>
             </main>
-        </div>
-    </body>
-</html>
+<?php include 'footer.php'; ?>
