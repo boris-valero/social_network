9a5cb8141d8e2a000a5f5e0c240ad48cc384ce1a
boris-valero <157314735+boris-valero@users.noreply.github.com>
2024-12-06 08:54:04 +0100

    Use a class for database operations

--- tags.php
+++ tags.php
@@ -1,29 +1,31 @@
-<?php $pageTitle = "Les mots-clés"; include 'header.php'; ?> 
-        <div id="wrapper">
-            <?php
-            $tagId = intval($_GET['tag_id']);
-            ?>
-            <?php
-            $mysqli = new mysqli("localhost", "root", "Jvale2lppsc", "socialnetwork");
-            ?>
-            <aside>
-                <?php
-                $laQuestionEnSql = "SELECT * FROM tags WHERE id= '$tagId' ";
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                $tag = $lesInformations->fetch_assoc();
-                ?>
-                <img src="user.jpg" alt="Portrait de l'utilisateur"/>
-                <section>
-                    <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez les derniers messages comportant
-                        le mot-clé <?php echo htmlspecialchars($tag['label']) ?>
-                        (n° <?php echo $tagId ?>)
-                    </p>
-                </section>
-            </aside>
-            <main>
-                <?php
-                $laQuestionEnSql = "
+<?php $pageTitle = "Les mots-clés";
+include 'header.php'; ?>
+<?php
+require_once 'config.php';
+require_once 'database.php';
+
+$db = new Database();
+$tagId = intval($_GET['tag_id']);
+?>
+<div id="wrapper">
+    <aside>
+        <?php
+        $laQuestionEnSql = "SELECT * FROM tags WHERE id= '$tagId' ";
+        $lesInformations = $mysqli->query($laQuestionEnSql);
+        $tag = $lesInformations->fetch_assoc();
+        ?>
+        <img src="user.jpg" alt="Portrait de l'utilisateur" />
+        <section>
+            <h3>Présentation</h3>
+            <p>Sur cette page vous trouverez les derniers messages comportant
+                le mot-clé <?php echo htmlspecialchars($tag['label']) ?>
+                (n° <?php echo $tagId ?>)
+            </p>
+        </section>
+    </aside>
+    <main>
+        <?php
+        $laQuestionEnSql = "
                     SELECT posts.content,
                     posts.created,
                     users.alias as author_name,  
@@ -39,36 +41,34 @@
                     GROUP BY posts.id
                     ORDER BY posts.created DESC  
                     ";
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                if ( ! $lesInformations)
-                {
-                    echo("Échec de la requete : " . $mysqli->error);
-                }
-                while ($post = $lesInformations->fetch_assoc())
-                {
-                    ?>                
-                    <article>
-                        <h3>
-                            <time datetime='<?php echo htmlspecialchars($post['created']); ?>'>
-                                <?php echo htmlspecialchars($post['created']); ?>
-                            </time>
-                        </h3>
-                        <address>par <?php echo htmlspecialchars($post['author_name']); ?></address>
-                        <div>
-                            <p><?php echo nl2br(htmlspecialchars($post['content'])); ?></p>
-                        </div>                                            
-                        <footer>
-                            <small><?php echo intval($post['like_number']); ?> J'aime</small>
-                            <?php 
-                            $tags = explode(',', $post['taglist']);
-                            foreach ($tags as $tag) {
-                                echo '<a href="#">' . htmlspecialchars($tag) . '</a> ';
-                            }
-                            ?>
-                        </footer>
-                    </article>
-                <?php 
-                } 
-                ?>
-            </main>
-<?php include 'footer.php'; ?>
+        $lesInformations = $mysqli->query($laQuestionEnSql);
+        if (!$lesInformations) {
+            echo ("Échec de la requete : " . $mysqli->error);
+        }
+        while ($post = $lesInformations->fetch_assoc()) {
+            ?>
+            <article>
+                <h3>
+                    <time datetime='<?php echo htmlspecialchars($post['created']); ?>'>
+                        <?php echo htmlspecialchars($post['created']); ?>
+                    </time>
+                </h3>
+                <address>par <?php echo htmlspecialchars($post['author_name']); ?></address>
+                <div>
+                    <p><?php echo nl2br(htmlspecialchars($post['content'])); ?></p>
+                </div>
+                <footer>
+                    <small><?php echo intval($post['like_number']); ?> J'aime</small>
+                    <?php
+                    $tags = explode(',', $post['taglist']);
+                    foreach ($tags as $tag) {
+                        echo '<a href="#">' . htmlspecialchars($tag) . '</a> ';
+                    }
+                    ?>
+                </footer>
+            </article>
+            <?php
+        }
+        ?>
+    </main>
+    <?php include 'footer.php'; ?>
