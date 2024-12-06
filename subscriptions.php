9c498a3f3a17267b676ebad32f27ab34579d1218
boris-valero <157314735+boris-valero@users.noreply.github.com>
2024-12-06 20:17:32 +0100

    Correction - Use a class for database operations

--- subscriptions.php
+++ subscriptions.php
@@ -1,47 +1,47 @@
-<?php $pageTitle = "Mes abonnés";
-include 'header.php'; ?>
+<?php $pageTitle = "Mes abonnés"; include 'header.php'; ?>
 <?php
 require_once 'config.php';
 require_once 'database.php';
 
 $db = new Database();
+$userId = intval($_GET['user_id']);
 ?>
 <div id="wrapper">
     <aside>
-        <img src="user.jpg" alt="Portrait de l'utilisateur" />
+        <img src="user.jpg" alt="Portrait de l'utilisateur"/>
         <section>
             <h3>Présentation</h3>
             <p>Sur cette page vous trouverez la liste des personnes dont
                 l'utilisateur n°
-                <?php echo intval($_GET['user_id']) ?>
+                <?php echo $userId ?>
                 suit les messages
             </p>
-
         </section>
     </aside>
     <main class='contacts'>
         <?php
-        $userId = intval($_GET['user_id']);
         $laQuestionEnSql = "
-                    SELECT users.* 
-                    FROM followers 
-                    LEFT JOIN users ON users.id=followers.followed_user_id 
-                    WHERE followers.following_user_id='$userId'
-                    GROUP BY users.id
-                    ";
-        $lesInformations = $mysqli->query($laQuestionEnSql);
+            SELECT users.* 
+            FROM followers 
+            LEFT JOIN users ON users.id=followers.followed_user_id 
+            WHERE followers.following_user_id='$userId'
+            GROUP BY users.id
+            ";
+        $lesInformations = $db->query($laQuestionEnSql);
         if (!$lesInformations) {
-            echo "Échec de la requête : " . $mysqli->error;
+            echo "Échec de la requête : " . $db->error;
+            exit();
         }
         while ($abonne = $lesInformations->fetch_assoc()) {
-            ?>
+            ?>                
             <article>
-                <img src="user.jpg" alt="blason" />
+                <img src="user.jpg" alt="blason"/>
                 <h3><?php echo htmlspecialchars($abonne['alias']); ?></h3>
-                <p>id:<?php echo intval($abonne['id']); ?></p>
+                <p>id:<?php echo intval($abonne['id']); ?></p>                    
             </article>
-        <?php
-        }
+        <?php 
+        } 
         ?>
     </main>
-    <?php include 'footer.php'; ?>
+</div>
+<?php include 'footer.php'; ?>
