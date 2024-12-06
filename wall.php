91aba13614e8cf8b4d87e9946517bcd8687ba959
boris-valero <157314735+boris-valero@users.noreply.github.com>
2024-12-06 20:19:41 +0100

    Correction - Use a class for database operations

--- wall.php
+++ wall.php
@@ -1,5 +1,4 @@
-<?php $pageTitle = "Mon mur";
-include 'header.php'; ?>
+<?php $pageTitle = "Mon mur"; include 'header.php'; ?> 
 <?php
 require_once 'config.php';
 require_once 'database.php';
@@ -11,14 +10,17 @@ $userId = intval($_GET['user_id']);
     <aside>
         <?php
         $laQuestionEnSql = "SELECT * FROM users WHERE id= '$userId' ";
-        $lesInformations = $mysqli->query($laQuestionEnSql);
+        $lesInformations = $db->query($laQuestionEnSql);
+        if (!$lesInformations) {
+            echo("Échec de la requête : " . $db->error);
+            exit();
+        }
         $user = $lesInformations->fetch_assoc();
         ?>
-        <img src="user.jpg" alt="Portrait de l'utilisateur" />
+        <img src="user.jpg" alt="Portrait de l'utilisateur"/>
         <section>
             <h3>Présentation</h3>
-            <p>Sur cette page vous trouverez tous les message de l'utilisateur :
-                <?php echo htmlspecialchars($user['alias']); ?>
+            <p>Sur cette page vous trouverez tous les messages de l'utilisateur : <?php echo htmlspecialchars($user['alias']); ?>
                 (n° <?php echo $userId ?>)
             </p>
         </section>
@@ -26,39 +28,40 @@ $userId = intval($_GET['user_id']);
     <main>
         <?php
         $laQuestionEnSql = "
-                    SELECT posts.content, posts.created, users.alias as author_name, 
-                    COUNT(likes.id) as like_number, GROUP_CONCAT(DISTINCT tags.label) AS taglist 
-                    FROM posts
-                    JOIN users ON  users.id=posts.user_id
-                    LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
-                    LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
-                    LEFT JOIN likes      ON likes.post_id  = posts.id 
-                    WHERE posts.user_id='$userId' 
-                    GROUP BY posts.id
-                    ORDER BY posts.created DESC  
-                    ";
-        $lesInformations = $mysqli->query($laQuestionEnSql);
+            SELECT posts.content, posts.created, users.alias as author_name, 
+            COUNT(likes.id) as like_number, GROUP_CONCAT(DISTINCT tags.label) AS taglist 
+            FROM posts
+            JOIN users ON  users.id=posts.user_id
+            LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
+            LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
+            LEFT JOIN likes      ON likes.post_id  = posts.id 
+            WHERE posts.user_id='$userId' 
+            GROUP BY posts.id
+            ORDER BY posts.created DESC  
+            ";
+        $lesInformations = $db->query($laQuestionEnSql);
         if (!$lesInformations) {
-            echo ("Échec de la requete : " . $mysqli->error);
+            echo("Échec de la requête : " . $db->error);
+            exit();
         }
         while ($post = $lesInformations->fetch_assoc()) {
-            ?>
+            ?>                
             <article>
                 <h3>
-                    <time><?php echo $post['created'] ?></time>
+                    <time><?php echo htmlspecialchars($post['created']); ?></time>
                 </h3>
-                <address><?php echo $post['author_name'] ?></address>
+                <address><?php echo htmlspecialchars($post['author_name']); ?></address>
                 <div>
-                    <div>
-                        <p><?php echo nl2br(htmlspecialchars($post['content'])); ?></p>
-                    </div>
-                    <footer>
-                        <small><?php echo intval($post['like_number']); ?> J'aime</small>
-                        <a href="#"><?php echo htmlspecialchars($post['taglist']); ?></a>
-                    </footer>
+                    <p><?php echo nl2br(htmlspecialchars($post['content'])); ?></p>
+                </div>                                            
+                <footer>
+                    <small><?php echo intval($post['like_number']); ?> J'aime</small>
+                    <a href="#"><?php echo htmlspecialchars($post['taglist']); ?></a>
+                </footer>
             </article>
-        <?php
-        }
+        <?php 
+        } 
         ?>
     </main>
-    <?php include 'footer.php'; ?>
+</div>
+<?php include 'footer.php'; ?>
