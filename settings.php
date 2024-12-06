5fb1112726a2e4a278eb7939427528cb7af558f9
boris-valero <157314735+boris-valero@users.noreply.github.com>
2024-12-06 20:12:19 +0100

    Correction - Use a class for database operations

--- settings.php
+++ settings.php
@@ -1,41 +1,41 @@
-<?php $pageTitle = "Mes paramètres";
-include 'header.php'; ?>
+<?php $pageTitle = "Mes paramètres"; include 'header.php'; ?>
 <?php
 require_once 'config.php';
 require_once 'database.php';
 
 $db = new Database();
+$userId = intval($_GET['user_id']);
 ?>
 <div id="wrapper" class='profile'>
     <aside>
-        <img src="user.jpg" alt="Portrait de l'utilisateur" />
+        <img src="user.jpg" alt="Portrait de l'utilisateur"/>
         <section>
             <h3>Présentation</h3>
             <p>Sur cette page vous trouverez les informations de l'utilisateur
-                n° <?php echo intval($_GET['user_id']) ?></p>
+                n° <?php echo $userId ?></p>
         </section>
     </aside>
     <main>
         <?php
-        $userId = intval($_GET['user_id']);
         $laQuestionEnSql = "
-                    SELECT users.*, 
-                    count(DISTINCT posts.id) as totalpost, 
-                    count(DISTINCT given.post_id) as totalgiven, 
-                    count(DISTINCT recieved.user_id) as totalrecieved 
-                    FROM users 
-                    LEFT JOIN posts ON posts.user_id=users.id 
-                    LEFT JOIN likes as given ON given.user_id=users.id 
-                    LEFT JOIN likes as recieved ON recieved.post_id=posts.id 
-                    WHERE users.id = '$userId' 
-                    GROUP BY users.id
-                    ";
-        $lesInformations = $mysqli->query($laQuestionEnSql);
+            SELECT users.*, 
+            count(DISTINCT posts.id) as totalpost, 
+            count(DISTINCT given.post_id) as totalgiven, 
+            count(DISTINCT recieved.user_id) as totalrecieved 
+            FROM users 
+            LEFT JOIN posts ON posts.user_id=users.id 
+            LEFT JOIN likes as given ON given.user_id=users.id 
+            LEFT JOIN likes as recieved ON recieved.post_id=posts.id 
+            WHERE users.id = '$userId' 
+            GROUP BY users.id
+            ";
+        $lesInformations = $db->query($laQuestionEnSql);
         if (!$lesInformations) {
-            echo ("Échec de la requete : " . $mysqli->error);
+            echo("Échec de la requête : " . $db->error);
+            exit();
         }
         $user = $lesInformations->fetch_assoc();
-        ?>
+        ?>                
         <article class='parameters'>
             <h3>Mes paramètres</h3>
             <dl>
@@ -44,7 +44,8 @@ $db = new Database();
                 <dt>Nombre de message</dt> : <dd><?php echo intval($user['totalpost']); ?></dd>
                 <dt>Nombre de "J'aime" donnés </dt> : <dd><?php echo intval($user['totalgiven']); ?></dd>
                 <dt>Nombre de "J'aime" reçus</dt> : <dd><?php echo intval($user['totalrecieved']); ?></dd>
-            </dl>
+             </dl>
         </article>
     </main>
-    <?php include 'footer.php'; ?>
+</div>
+<?php include 'footer.php'; ?>
