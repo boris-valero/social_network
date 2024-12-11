ec1233746c155d366a1af70ad23d56b59813d0bb
boris-valero <val_b@orange.fr>
2024-12-11 19:05:41 +0100

    Adding the link to user's wall in Feed.php

--- feed.php
+++ feed.php
@@ -31,9 +31,11 @@ $userId = intval($_GET['user_id']);
         $laQuestionEnSql = "
             SELECT posts.content,
             posts.created,
+            posts.user_id,
             users.alias as author_name,  
             count(likes.id) as like_number,  
-            GROUP_CONCAT(DISTINCT tags.label) AS taglist 
+            GROUP_CONCAT(DISTINCT tags.label ORDER BY tags.label) AS taglist,
+            GROUP_CONCAT(DISTINCT tags.id ORDER BY tags.label) AS tag_ids
             FROM followers 
             JOIN users ON users.id=followers.followed_user_id
             JOIN posts ON posts.user_id=users.id
@@ -55,13 +57,24 @@ $userId = intval($_GET['user_id']);
                 <h3>
                     <time><?php echo htmlspecialchars($post['created']); ?></time>
                 </h3>
-                <address><?php echo htmlspecialchars($post['author_name']); ?></address>
+                <address>
+                    <a href="wall.php?user_id=<?php echo intval($post['user_id']); ?>"><?php echo htmlspecialchars($post['author_name']); ?></a>
+                </address>
                 <div>
                     <p><?php echo nl2br(htmlspecialchars($post['content'])); ?></p>
                 </div>                                            
                 <footer>
                     <small><?php echo intval($post['like_number']); ?> J'aime</small>
-                    <a href="#"><?php echo htmlspecialchars($post['taglist']); ?></a>
+                    <small style="float: right;">
+                        <?php
+                        $tags = explode(',', $post['taglist']);
+                        $tag_ids = explode(',', $post['tag_ids']);
+                        foreach ($tags as $index => $tag) {
+                            $tag_id = intval($tag_ids[$index]);
+                            echo '<a href="tag.php?id=' . $tag_id . '">' . htmlspecialchars($tag) . '</a> ';
+                        }
+                        ?>
+                    </small>
                 </footer>
             </article>
         <?php 
