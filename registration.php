c1f308acd64cfee67a2842381e93b29a02ea79f7
Mon Ordinateur <ordinateur@core>
2025-01-10 13:47:51 +0100

    Add form in Login.php | regristration.php | usurpedpost.php

--- registration.php
+++ registration.php
@@ -22,7 +22,6 @@
                     <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
                     <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
                 </ul>
-
             </nav>
         </header>
 
@@ -36,42 +35,26 @@
                 <article>
                     <h2>Inscription</h2>
                     <?php
-                    /**
-                     * TRAITEMENT DU FORMULAIRE
-                     */
-                    // Etape 1 : vérifier si on est en train d'afficher ou de traiter le formulaire
-                    // si on recoit un champs email rempli il y a une chance que ce soit un traitement
+                    require_once 'config.php';
+
                     $enCoursDeTraitement = isset($_POST['email']);
                     if ($enCoursDeTraitement)
                     {
-                        // on ne fait ce qui suit que si un formulaire a été soumis.
-                        // Etape 2: récupérer ce qu'il y a dans le formulaire @todo: c'est là que votre travaille se situe
-                        // observez le résultat de cette ligne de débug (vous l'effacerez ensuite)
-                        echo "<pre>" . print_r($_POST, 1) . "</pre>";
-                        // et complétez le code ci dessous en remplaçant les ???
-                        $new_email = $_POST['???'];
-                        $new_alias = $_POST['???'];
-                        $new_passwd = $_POST['???'];
+                        $new_email = $_POST['email'];
+                        $new_alias = $_POST['pseudo'];
+                        $new_passwd = $_POST['motpasse'];
 
-
-                        //Etape 3 : Ouvrir une connexion avec la base de donnée.
-                        $mysqli = new mysqli("localhost", "root", "root", "socialnetwork_tests");
-                        //Etape 4 : Petite sécurité
-                        // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
+                        $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
                         $new_email = $mysqli->real_escape_string($new_email);
                         $new_alias = $mysqli->real_escape_string($new_alias);
                         $new_passwd = $mysqli->real_escape_string($new_passwd);
-                        // on crypte le mot de passe pour éviter d'exposer notre utilisatrice en cas d'intrusion dans nos systèmes
                         $new_passwd = md5($new_passwd);
-                        // NB: md5 est pédagogique mais n'est pas recommandée pour une vraies sécurité
-                        //Etape 5 : construction de la requete
                         $lInstructionSql = "INSERT INTO users (id, email, password, alias) "
                                 . "VALUES (NULL, "
                                 . "'" . $new_email . "', "
                                 . "'" . $new_passwd . "', "
                                 . "'" . $new_alias . "'"
                                 . ");";
-                        // Etape 6: exécution de la requete
                         $ok = $mysqli->query($lInstructionSql);
                         if ( ! $ok)
                         {
@@ -84,14 +67,14 @@
                     }
                     ?>                     
                     <form action="registration.php" method="post">
-                        <input type='hidden'name='???' value='achanger'>
+                        <input type='hidden' name='form_submitted' value='1'>
                         <dl>
                             <dt><label for='pseudo'>Pseudo</label></dt>
-                            <dd><input type='text'name='pseudo'></dd>
+                            <dd><input type='text' name='pseudo'></dd>
                             <dt><label for='email'>E-Mail</label></dt>
-                            <dd><input type='email'name='email'></dd>
+                            <dd><input type='email' name='email'></dd>
                             <dt><label for='motpasse'>Mot de passe</label></dt>
-                            <dd><input type='password'name='motpasse'></dd>
+                            <dd><input type='password' name='motpasse'></dd>
                         </dl>
                         <input type='submit'>
                     </form>
